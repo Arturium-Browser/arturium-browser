1f37154a5c9a76c77cbd2e63bd22a70ea3211cb3
Arturman_RU <123005948+ArturmanRU@users.noreply.github.com>
2023-01-18 17:55:43 +0000

    Initial commit

--- arturium-browser /arturium-browser.c
+++ arturium-browser /arturium-browser.c
@@ -1,3 +1,4 @@
+#define browser
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,7 +6,6 @@
 #ifdef __windows__
 #   error "Your system is not supported :("
 #endif
-#define browser
 int main(void)
 {
     char * const run[] = {"python3 ../../opt/arturium-browser/resources/plugin.py &> /tmp/arturium-browser.log || echo $(cat /tmp/arturium-browser.log). Exiting"};
