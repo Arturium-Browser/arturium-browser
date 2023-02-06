28d7a70065139c4933d6ac0a8df49cccc651f341
Arturium Browser <123005948+Arturium-Browser@users.noreply.github.com>
2023-02-06 16:06:23 +0000

    Update arturium-browser.c
    Signed-off-by: Arturium Browser <123005948+Arturium-Browser@users.noreply.github.com>

--- arturium-browser/arturium-browser.c
+++ arturium-browser/arturium-browser.c
@@ -25,11 +25,6 @@
 int main(int argc, char *argv[])
 {
 char *browser[] = {"python3 /opt/arturium-browser/resources/plugin.py &> /tmp/arturium.log || echo $(cat /tmp/arturium.log). Exiting ; rm /tmp/arturium.log 2>/dev/null"};
-// char *id = {"$(id -u)"};
-// if(id=0) {
-//     printf("Cannot start %s with root", argv[0]);
-//     return 1;
-// }
 
 if (geteuid() == 0) {
     printf("Cannot start %s with root\n\r", argv[0]);
@@ -38,7 +33,7 @@ if (geteuid() == 0) {
 
 if(argc<2) {
     system(browser[0]);
-    return 0;
+    exit(0);
 }
 
 if(argc>2) {
@@ -46,21 +41,21 @@ if(argc>2) {
 }
 
 if(!strcmp(argv[1], "--version")) {
-    printf("  %s  v0.287.2", argv[0]);
+    printf("  %s  v0.287.2\n\r", argv[0]);
     system("echo 'License: GNU Generic Public License v3'");
-    return 0;
+    exit(0);
 }
 
 if(!strcmp(argv[1], "--help")) {
     system("echo 'Supported options:'");
     system("echo '    --help                  This help screen'");
     system("echo '    --version               Show version and exit'");
-    return 0;
+    exit(0);
 }
 
 if(!strcmp(argv[1], argv[1])) {
-    printf("Unsupported argument: %s", argv[1]);
-    return 1;
+    printf("Unsupported argument: %s\n\r", argv[1]);
+    exit(1);
 }
 
 }
